9dd2a03ac4640fd660da3c991aff51b4465e266f
Geerocktricks <geerockface4@gmail.com>
2018-10-12 00:51:58 +0300

    This commit will create my address constructor

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// back-end
+// constructor--------------------------
+function Address(firstName,lastName,contact,location){
+  this.firstName=firstName;
+  this.lastName=lastName;
+  this.contact=contact;
+  this.location=location;
+};
+var Kevine=new Address("Kevine", "Igihonzi",07884714088,"Langata");
+console.log(Kevine);
